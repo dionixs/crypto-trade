13073b22cf586da869a713ea972f99c733c07c08
di0nixs <di0nixs@protonmail.com>
2021-10-11 19:57:11 +0300

    create: add section main

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click", function () {
-  console.log("Клик по кнопке");
-  document
-    .querySelector(".navbar-mobile")
-    .classList.toggle("navbar-mobile--visible");
-});
