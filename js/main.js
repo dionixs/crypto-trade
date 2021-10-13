22b698266e80c6d026f5ddb3424ec9daf9147d27
di0nixs <di0nixs@protonmail.com>
2021-10-13 12:25:08 +0300

    create: add slider

--- js/main.js
+++ js/main.js
@@ -5,3 +5,48 @@ menuBtn.addEventListener("click", function () {
     .classList.toggle("navbar-mobile--visible");
 });
 
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 35,
+  loopFillGroupWithBlank: true,
+  setWrapperSize: true,
+
+  direction: "horizontal",
+  loop: true,
+
+  navigation: {
+    nextEl: ".reviews-btn-next",
+    prevEl: ".reviews-btn-prev",
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 35,
+    },
+  }
+});
