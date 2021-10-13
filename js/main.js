0c5113b6b8a606ae6e1af42dd5aba9f2a3527e19
di0nixs <di0nixs@protonmail.com>
2021-10-13 15:49:24 +0300

    create: add section questions

--- js/main.js
+++ js/main.js
@@ -1,52 +1,60 @@
-var menuBtn = document.querySelector(".menu-btn");
-menuBtn.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-mobile")
-    .classList.toggle("navbar-mobile--visible");
-});
+$(document).ready(function () {
+  var menuBtn = document.querySelector(".menu-btn");
+  menuBtn.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-mobile")
+      .classList.toggle("navbar-mobile--visible");
+  });
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 35,
-  loopFillGroupWithBlank: true,
-  setWrapperSize: true,
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    loopFillGroupWithBlank: true,
+    setWrapperSize: true,
 
-  direction: "horizontal",
-  loop: true,
+    direction: "horizontal",
+    loop: true,
 
-  navigation: {
-    nextEl: ".reviews-btn-next",
-    prevEl: ".reviews-btn-prev",
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-
-  autoplay: {
-    delay: 5000,
-  },
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0
+    navigation: {
+      nextEl: ".reviews-btn-next",
+      prevEl: ".reviews-btn-prev",
     },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 0
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
     },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 0
+
+    autoplay: {
+      delay: 5000,
     },
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 35,
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
     },
-  }
+  });
+
+  $(".questions-block__title").click(function () {
+    $(this).next().toggleClass("questions-block__ul--visible");
+    $(this).child().toggleClass("dropdown-arrow--rotate");
+  });
 });
